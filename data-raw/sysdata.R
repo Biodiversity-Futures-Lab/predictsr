10c3bc81c3e56ba04d4f9246b883f25a81ed86fc
Connor Duffin <connor.p.duffin@gmail.com>
2025-05-01 14:42:23 +0100

    Scrips to generate sysdata.rda

---
+++ data-raw/sysdata.R
@@ -0,0 +1,11 @@
+base_url <- "https://data.nhm.ac.uk"
+package_id_2016 <- "4e3a9108-3e25-43d8-9f58-31b40fe438d6"
+package_id_2022 <- "d2bf57f3-ea67-469d-afd4-0b6416be9af2"
+
+usethis::use_data(
+  base_url,
+  package_id_2016,
+  package_id_2022,
+  overwrite = TRUE,
+  internal = TRUE
+)
