0bf8d65f13c3d46c513bf83eef5e9909ef0c4cbd
Connor Duffin <connor.p.duffin@gmail.com>
2025-07-01 15:58:24 +0100

    Prepare for version 0.1.0 release
    - Update DESCRIPTION with new version number
    - Add release notes for version 0.1.0 in NEWS.md
    - Update cran-comments.md to reflect resolved note
    - Remove unused variable `base_url` from sysdata.R

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,5 @@
+# predictsr 0.1.0
+
 # predictsr 0.0.1
 
 * Initial CRAN submission.
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,8 +1,6 @@
-base_url <- "https://data.nhm.ac.uk"
 download_url <- "https://data.nhm.ac.uk/api/3/action/vds_download_queue"
 
 usethis::use_data(
-  base_url,
   download_url,
   overwrite = TRUE,
   internal = TRUE
