1606c297ebe5433d02bfe277419cd03ac740865a
Connor Duffin <connor.p.duffin@gmail.com>
2025-07-23 16:47:48 +0100

    Fix line wrapping in license descriptions for clarity

--- R/predictsr.R
+++ R/predictsr.R
@@ -81,8 +81,8 @@ GetPredictsData <- function(extract = c(2016, 2022)) {
 #'
 #' The data are provided under a CC NC (non-commercial) license, which means
 #'   that they cannot be used for commercial purposes. The 2016 release is
-#'   available under a CC BY-NC-SA 4.0 license, and the 2022 release is available
-#'   under a CC NC (any) license.
+#'   available under a CC BY-NC-SA 4.0 license, and the 2022 release is
+#'   available under a CC NC (any) license.
 #'
 #' @param extract Numeric, year/s corresponding to PREDICTS database releases to
 #'   download. Options are 2016 or 2022. Defaults to `c(2016, 2022)` - the whole
@@ -144,8 +144,8 @@ GetSitelevelSummaries <- function(extract = c(2016, 2022)) {
 #'
 #' The data are provided under a CC NC (non-commercial) license, which means
 #'   that they cannot be used for commercial purposes. The 2016 release is
-#'   available under a CC BY-NC-SA 4.0 license, and the 2022 release is available
-#'   under a CC NC (any) license.
+#'   available under a CC BY-NC-SA 4.0 license, and the 2022 release is
+#'   available under a CC NC (any) license.
 #'
 #' @param ... extra arguments passed to read.csv.
 #' @returns The column descriptions in the format as a dataframe.
